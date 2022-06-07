1e24ff518655b7e903fe3cfe847315b5fee1334f
miker704 <miker704@gmail.com>
2022-06-07 09:49:47 -0400

    established project repo and structure

---
+++ todo_react/frontend/todo_redux.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    ReactDOM.render(
+        <h1>Todo App in React && Redux</h1>,
+        document.getElementById('content')
+    );
+})
