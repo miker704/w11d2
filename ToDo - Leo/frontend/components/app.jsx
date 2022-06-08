6c92dc7d950c23a65cf06dda1691bcaf0dcec560
Leo Chung <leo.chung97@gmail.com>
2022-06-08 10:24:32 -0400

    Phase 2 Start

---
+++ ToDo - Leo/frontend/components/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <div className="app">
+    <h1>To Do List</h1>
+  </div>
+)
+
+export default App;
--- ToDo - Leo/frontend/todo_redux.jsx
+++ ToDo - Leo/frontend/todo_redux.jsx
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store.js';
-import { receiveTodo, receiveTodos, removeTodo } from './actions/todo_actions.js';
-import { receiveStep, receiveSteps, removeStep } from './actions/step_actions.js';
+import Root from './components/root.jsx';
 
 document.addEventListener("DOMContentLoaded", () => {
-  const store = configureStore();
+  const store = configureStore(); 
   
-  window.store = store;
-  window.receiveTodos = receiveTodos;
-  window.receiveTodo = receiveTodo;
-  window.removeTodo = removeTodo;
-
-  window.receiveStep = receiveStep;
-  window.receiveSteps = receiveSteps;
-  window.removeStep = removeStep;
+  const root = document.querySelector('#content');
+  ReactDOM.render(<Root store={store} />, root);
 });
